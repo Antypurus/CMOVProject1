ac68acbbb64e3d768af63240e3c11cf8ed15e80c
Tiago Jose Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2019-10-28 21:16:21 +0000

    definied initial database schema

--- Server/DB/schema.sql
+++ Server/DB/schema.sql
@@ -13,3 +13,28 @@ create table Client(
 	current_accumulated_euro INTEGER not null default 0,
 	current_accumulated_cent INTEGER not null default 0
 );
+
+drop table if exists Voucher cascade;
+create table Voucher(
+	id uuid primary key default uuid_generate_v4(),
+	user uuid not null REFERENCES Client(id),
+	was_used BOOLEAN not null DEFAULT FALSE
+);
+
+drop table if exists Purchase cascade;
+create table Purchase(
+	id uuid primary key default uuid_generate_v4(),
+	user uuid not null REFERENCES Client(id),
+	voucher uuid REFERENCES Voucher(id) DEFAULT NULL,
+	should_discount BOOLEAN not null DEFAULT false
+);
+
+drop table if exists Product cascade;
+create table Product(
+	id uuid primary key default uuid_generate_v4(),
+	price_euro INTEGER not null,
+	price_cent INTEGER not null,
+	name text not null,
+	image_url text DEFAULT NULL,
+	purchase uuid REFERENCES Purchase(id) DEFAULT null
+);
