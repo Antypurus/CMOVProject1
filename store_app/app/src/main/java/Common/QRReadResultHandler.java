20f9bd213c1ecc0fd544ae65dfe9d91797227680
Tiago Jose Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2019-11-08 22:32:18 +0000

    created sollution for handling QR code reading cleanly

---
+++ store_app/app/src/main/java/Common/QRReadResultHandler.java
@@ -0,0 +1,5 @@
+package Common;
+
+public interface QRReadResultHandler {
+    public void Handler(String data);
+}
--- store_app/app/src/main/java/com/example/storeapplication/MainActivity.java
+++ store_app/app/src/main/java/com/example/storeapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.storeapplication;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -30,74 +32,39 @@ import java.security.NoSuchProviderException;
 import java.security.UnrecoverableEntryException;
 import java.security.cert.CertificateException;
 
+import Common.QR;
+import Common.QRActivity;
+import Common.QRReadResultHandler;
 import Common.RSA;
 
-public class MainActivity extends AppCompatActivity {
-
-    public static int white = 0xFFFFFFFF;
-    public static int black = 0xFF000000;
-
-    private static Bitmap generateQRCodeImage(String text, int width, int height, String filePath)
-            throws WriterException {
-        QRCodeWriter qrCodeWriter = new QRCodeWriter();
-        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
-
-        int[] pixels = new int[width * height];
-        for (int y = 0; y < height; y++) {
-            int offset = y * width;
-            for (int x = 0; x < width; x++) {
-                pixels[offset + x] = bitMatrix.get(x, y) ? black : white;
-            }
-        }
-        Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        bmp.setPixels(pixels, 0, width, 0, 0, width, height);
-        return bmp;
-    }
+public class MainActivity extends QRActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Bitmap data = null;
+        ImageView image = (ImageView) findViewById(R.id.image);
         try {
-            data = generateQRCodeImage("hello world", 480, 480, "");
+            image.setImageBitmap(QR.GenerateQRCode("testing"));
         } catch (WriterException e) {
             e.printStackTrace();
         }
 
-        ImageView image = (ImageView) findViewById(R.id.image);
-        image.setImageBitmap(data);
-
+        final Activity current = this;
         image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startQRScanner();
+                QRCodeReader.StartQRScanner(current, (QRReadResultHandler) current);
             }
         });
         TextView text = findViewById(R.id.decode);
     }
 
-    private void startQRScanner() {
-        new IntentIntegrator(this).initiateScan();
-    }
-
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if (result != null) {
-            if (result.getContents() == null) {
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
-            } else {
-                updateText(result.getContents());
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
+    public void Handler(String data) {
+        TextView text = findViewById(R.id.decode);
+        text.setText(data);
     }
-
-    private void updateText(String scanCode) {
-    }
-
 }
 
