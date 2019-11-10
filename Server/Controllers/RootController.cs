1cab4a6e919da0641b2bbf790d9ece40ebf0ed2c
Tiago Jose Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2019-11-10 00:42:31 +0000

    implemented server side registration

--- Server/Controllers/RootController.cs
+++ Server/Controllers/RootController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
+using Server.Models;
 using Server.Utils;
 
 namespace Server.Controllers
@@ -20,12 +21,15 @@ namespace Server.Controllers
             return "Alive";
         }
 
-        [HttpPost("test")]
-        public string test([FromBody]JObject data)
+        [HttpPost("register")]
+        public Guid register([FromBody]JObject data)
         {
-            string name = data["name"].ToObject<string>();
-            string job = data["job"].ToString();
-            return name+job;
+            string name = data["name"].ToString();
+            string username = data["username"].ToString();
+            string password = data["password"].ToString();
+            int credit = data["credit_card_no"].ToObject<int>();
+            string key = data["public_key"].ToString();
+            return Client.RegisterUser(name,username,password,key,credit).GetClientID();
         }
     }
 }
---
+++ Server/Models/Client.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Server.Utils;
+
+namespace Server.Models
+{
+    public class Client
+    {
+        private Guid ClientID;
+
+        public Client(Guid clientID)
+        {
+            this.ClientID = clientID;
+        }
+
+        public Client(string clientID)
+        {
+            this.ClientID = Guid.Parse(clientID);
+        }
+
+        public Guid GetClientID()
+        {
+            return this.ClientID;
+        }
+
+        public static Client RegisterUser(string name, string username, string password, string public_key, int credit_card)
+        {
+            Database db = Database.GetDatabase();
+            List<Entry> entries = new List<Entry>();
+            entries.Add(new Entry { name = "name", value = name });
+            entries.Add(new Entry { name = "username", value = username });
+            entries.Add(new Entry { name = "password", value = password });
+            entries.Add(new Entry { name = "credit", value = credit_card });
+            entries.Add(new Entry { name = "key", value = public_key });
+            Guid userID = (Guid)db.InsertWithReturn("insert into " +
+                "Client(name,username,password,credit_card,public_key) " +
+                "values(@name,@username,@password,@credit,@key) returning id;", entries);
+            Client client = new Client(userID);
+            return client;
+        }
+    }
+}
