fbe5d2ae07b320b6a28553157cbf0124f64f6aec
Tiago Jose Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2019-11-11 16:00:32 +0000

    working on registed activity

--- store_app/app/src/main/java/com/example/storeapplication/Activities/Client/RegisterActivity.java
+++ store_app/app/src/main/java/com/example/storeapplication/Activities/Client/RegisterActivity.java
@@ -1,18 +1,72 @@
 package com.example.storeapplication.Activities.Client;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.storeapplication.R;
 
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+import Common.Constants;
+import Common.HTTP.HTTP;
+import Common.HTTP.HTTPResultHandler;
+import Common.RSA;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    private TextView name;
+    private TextView username;
+    private TextView password;
+    private TextView credit_card;
+    private Button register_button;
 
     @Override
     public void onCreate(Bundle savedInstances)
     {
         super.onCreate(savedInstances);
         setContentView(R.layout.register_view);
+
+        this.name = findViewById(R.id.name);
+        this.username = findViewById(R.id.username);
+        this.password = findViewById(R.id.password);
+        this.credit_card = findViewById(R.id.card_number);
+        this.register_button = findViewById(R.id.register_button);
+
+        this.register_button.setOnClickListener(view->register());
+
+    }
+
+    public void register()
+    {
+        try {
+            RSA.GeneratePublicPrivateKeyPair(this);
+            HashMap<String, String> body = new HashMap<>();
+            body.put("name", (String) this.name.getText());
+            body.put("username", (String) this.username.getText());
+            body.put("password", (String) this.password.getText());
+            body.put("credit_card_no", (String) this.credit_card.getText());
+            body.put("public_key", RSA.getPublicKey());
+            HTTP.PostRequest(Constants.Register_Route, null, body, new HTTPResultHandler() {
+                @Override
+                public void Handler(Object result) {
+                    try {
+                        JSONObject response = new JSONObject((String) result);
+                        //response.getString("server_key");
+                        Log.d("register",response.getString("server_key"));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
