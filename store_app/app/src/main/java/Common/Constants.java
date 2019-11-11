d5d7ac9946b217cf45b1425b3c09159fcf829ff3
Tiago Jose Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2019-11-11 15:44:47 +0000

    added a checkout view

---
+++ store_app/app/src/main/java/Common/Constants.java
@@ -0,0 +1,9 @@
+package Common;
+
+public class Constants {
+
+    public static final String SERVER_IP = "10.0.0.5";
+    public static final String Register_Route = SERVER_IP + "/register";
+    public static final String Product_List_Route = SERVER_IP + "/products";
+
+}
