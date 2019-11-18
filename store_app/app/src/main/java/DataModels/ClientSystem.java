0b529f3c3406c947d34a0a51626ccc842f372218
Tiago Jose Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2019-11-18 17:24:04 +0000

    added user data fetching to the client system class

--- store_app/app/src/main/java/DataModels/ClientSystem.java
+++ store_app/app/src/main/java/DataModels/ClientSystem.java
@@ -1,10 +1,13 @@
 package DataModels;
 
 import android.app.Application;
+import android.content.SharedPreferences;
 
 public class ClientSystem extends Application {
 
     private String ClientUsername = "";
+    private String ServerKey = "";
+    private String ClientUserID = "";
     private static ClientSystem system = null;
 
     public ClientSystem(String username)
@@ -12,6 +15,14 @@ public class ClientSystem extends Application {
         this.ClientUsername = username;
         system = this;
     }
+
+    private void fetchUserDate()
+    {
+        SharedPreferences preferences = getSharedPreferences(this.ClientUsername,MODE_PRIVATE);
+        this.ServerKey = preferences.getString("Server_Key","");
+        this.ClientUserID = preferences.getString("user_id","");
+    }
+
 
     public ClientSystem GetSystem()
     {
