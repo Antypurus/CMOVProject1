ead60f2043d34ac2e9c84a4c57485c846302ee83
Tiago Jose Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2019-11-20 10:07:57 +0000

    working on coupon list

--- store_app/app/src/main/java/com/example/storeapplication/Activities/Client/CouponView/CouponAdapter.java
+++ store_app/app/src/main/java/com/example/storeapplication/Activities/Client/CouponView/CouponAdapter.java
@@ -1,4 +1,39 @@
 package com.example.storeapplication.Activities.Client.CouponView;
 
-public class CouponAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.storeapplication.R;
+
+public class CouponAdapter extends  RecyclerView.Adapter<CouponAdapter.CouponViewHolder>{
+
+    public static class CouponViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView id;
+        public CouponViewHolder(@NonNull View itemView) {
+            super(itemView);
+            id = itemView.findViewById(R.id.coupon_id);
+        }
+    }
+
+    @NonNull
+    @Override
+    public CouponViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CouponViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
 }
